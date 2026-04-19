729d4755cf800545b5507d984ef7461cbdb57153
Louis Moulin <moulin.louis93@gmail.com>
2026-04-19 14:54:59 +0000

    feat(rta): button presence detector

--- rta/src/poker_rta/cv/__init__.py
+++ rta/src/poker_rta/cv/__init__.py
@@ -1,4 +1,11 @@
+from poker_rta.cv.buttons import ButtonDetector
 from poker_rta.cv.cards import CardClassifier, classify_card
 from poker_rta.cv.ocr import NumberReader, parse_chip_amount
 
-__all__ = ["CardClassifier", "NumberReader", "classify_card", "parse_chip_amount"]
+__all__ = [
+    "ButtonDetector",
+    "CardClassifier",
+    "NumberReader",
+    "classify_card",
+    "parse_chip_amount",
+]
